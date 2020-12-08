2e98efb9a8eb1afdadf47ad408a184236cac380d
jamesleiner <59890482+jamesleiner@users.noreply.github.com>
2020-12-07 21:47:18 -0600

    changing parameters around

--- All Methods Loop.R
+++ All Methods Loop.R
@@ -4,14 +4,15 @@ library(doMC)
 source("kada_code.R")
 source("loop_functions.R")
 
-nlist <- c(1000,5000)
-ndivplist <- c(0.1,0.25)
+
+nlist <- c(1000,5000,6000)
+ndivplist <- c(0.1,0.15,0.25)
 slist <- c(0.1,0.25,0.5)
-rholist <- c(0,0.1,0.2,0.3,0.4,0.5)
+rholist <- c(0,0.025,0.05,0.075,0.1,0.2,0.3,0.4,0.5)
 amplitudelist <- c(5)
 ko_stat = stat.glmnet_lambdasmax
 iter_list <- expand.grid(nlist,ndivplist,slist,rholist,amplitudelist)
-iter_list <- iter_list[order(iter_list[,1],-iter_list[,2],-iter_list[,3]),]
+iter_list <- iter_list[order(iter_list[,4],iter_list[,1],-iter_list[,2],-iter_list[,3]),]
 
 outfile="results.csv"
 ntrials = 50
